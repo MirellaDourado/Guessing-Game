82e5c160574415c82d45766394d97b853de0484b
mirelladourado <mirellaalvesdourado@gmail.com>
2023-05-26 23:27:20 -0300

    feat: renomeando variavel da Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
   public static void main(String[] args) throws Exception {
-    GuessingGame teste = new GuessingGame(100, 1, 10);
-    teste.startGame();
+    GuessingGame game = new GuessingGame(100, 1, 10);
+    game.startGame();
   }
 }

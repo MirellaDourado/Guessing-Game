f7ed2b0042139a6131d3069a4fb2c5904dd42739
mirelladourado <mirellaalvesdourado@gmail.com>
2023-05-26 22:19:57 -0300

    feat: criacao dos getters e setters da classe GuessingGame

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -14,4 +14,28 @@ public class GuessingGame {
     this._attempts = attempts;
     this._scanner = new Scanner(System.in);
   }
+
+  public void setMaxNumber(int maxNumber) {
+    this._maxNumber = maxNumber;
+  }
+
+  public void setMinNumber(int minNumber) {
+    this._minNumber = minNumber;
+  }
+
+  public int getMaxNumber() {
+    return _maxNumber;
+  }
+
+  public int getMinNumber() {
+    return _minNumber;
+  }
+
+  public void setRandomNumber() {
+    this._randomNumber = (int)Math.floor(Math.random() * (this._maxNumber - this._minNumber + 1) + this._minNumber);
+  }
+
+  public int getRandomNumber() {
+    return this._randomNumber;
+  }
 }
